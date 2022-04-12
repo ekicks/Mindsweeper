b6228d1cccb8db74fabd365dc888ff9337a4f9ef
Ethan Ortega <EOrtega@student.fullsail.edu>
2022-04-11 20:33:00 -0400

    "Claculator Interface done"

--- Minesweeper/Main.cpp
+++ Minesweeper/Main.cpp
@@ -3,87 +3,39 @@ wxBEGIN_EVENT_TABLE(Main, wxFrame)
 
 wxEND_EVENT_TABLE()
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Main", wxPoint(40, 40), wxSize(700, 700)) {
-	button = new wxButton * [fWidth * fHeight];
-	wxGridSizer* fGrid = new wxGridSizer(fWidth, fHeight, 0, 0);
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Main", wxPoint(40, 40), wxSize(408, 580)) {
 
-	fField = new int[fWidth * fHeight];
+	button0 = new wxButton(this, 100, "0", wxPoint(1,300), wxSize(90, 60));
+	button1 = new wxButton(this, 101, "1", wxPoint(91,300), wxSize(90, 60));
+	button2 = new wxButton(this, 102, "2", wxPoint(181,300), wxSize(90, 60));
+	button3 = new wxButton(this, 103, "3", wxPoint(1,360), wxSize(90, 60));
+	button4 = new wxButton(this, 104, "4", wxPoint(91,360), wxSize(90, 60));
+	button5 = new wxButton(this, 105, "5", wxPoint(181,360), wxSize(90, 60));
+	button6 = new wxButton(this, 106, "6", wxPoint(1,420), wxSize(90, 60));
+	button7 = new wxButton(this, 107, "7", wxPoint(91,420), wxSize(90, 60));
+	button8 = new wxButton(this, 108, "8", wxPoint(181,420), wxSize(90, 60));
+	button9 = new wxButton(this, 109, "9", wxPoint(91,480), wxSize(90, 60));
+	buttonEquals = new wxButton(this, 110, "=", wxPoint(181,480), wxSize(90, 60));
+	buttonDot = new wxButton(this, 111, ".", wxPoint(1,480), wxSize(90, 60));
+	buttonClear = new wxButton(this, 112, "CLEAR", wxPoint(271,480), wxSize(120, 60));
+	buttonAdd = new wxButton(this, 113, "+", wxPoint(271,420), wxSize(120, 60));
+	buttonSubtract = new wxButton(this, 114, "-", wxPoint(271,360), wxSize(120, 60));
+	buttonMulti = new wxButton(this, 115, "*", wxPoint(271,300), wxSize(120, 60));
+	buttonDivide = new wxButton(this, 116, "÷", wxPoint(271,240), wxSize(120, 60));
+	buttonMod = new wxButton(this, 117, "%", wxPoint(271,180), wxSize(120, 60));
+	buttonBinary = new wxButton(this, 118, "BINARY", wxPoint(191,120), wxSize(200, 60));
+	buttonHex = new wxButton(this, 119, "HEX", wxPoint(1,120), wxSize(190, 60));
+	text = new wxTextCtrl(this, wxID_ANY, "0", wxPoint(1, 180), wxSize(270, 120),wxALIGN_RIGHT);
+	header = new wxStaticText(this, wxID_ANY, "CALCULATOR", wxPoint(1, 1), wxSize(408, 120),wxALIGN_CENTER);
 
-	wxFont font(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_EXTRABOLD, false);
-
-	for (int x = 0; x < fWidth; x++)
-	{
-		for (int y = 0; y < fHeight; y++)
-		{
-			button[y * fWidth + x] = new wxButton(this, 10000 + (y * fWidth + x));
-			button[y * fWidth + x]->SetFont(font);
-			fGrid->Add(button[y * fWidth + x], 1, wxEXPAND | wxALL);
-
-			button[y * fWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonClick, this);
-			fField[y * fWidth + x] = 0;
-		}
-	}
-	this->SetSizer(fGrid);
-	fGrid->Layout();
+	//SetBackgroundColour(wxColor(153, 204, 255));
+	
 }
 Main::~Main() {
-	delete[]button;
+	delete[]button0;
+	delete[]text;
 }
 void Main::OnButtonClick(wxCommandEvent& evt) {
 	
-	int x = (evt.GetId() - 10000) % fWidth;
-	int y = (evt.GetId() - 10000) / fWidth;
-
-	if (buttonClick1) {
-		int mines = 30;
-		while (mines) {
-			int randomX = rand() % fWidth;
-			int randomY = rand() % fHeight;
-
-			if(fField[randomY * fWidth + randomX] == 0 && randomX != x && randomY != y) {
-
-				fField[randomY * fWidth + randomX] = -1;
-				mines--;
-			}
-		}
-		buttonClick1 = false;
-	}
-	button[y * fWidth + x]->Enable(false);
-
-	if (fField[y * fWidth + x] == -1) {
-		wxMessageBox("Explosion Sound ! - GAME OVER ");
-
-		buttonClick1 = true;
-		for (int x = 0; x < fWidth; x++)
-		{
-			for (int y = 0; y < fHeight; y++)
-			{
-				fField[y * fWidth + x] = 0;
-				button[y * fWidth + x]->SetLabel("");
-				button[y * fWidth + x]->Enable(true);
-			}
-		}
-	}
-	else {
-		int surroundingMines = 0;
-		for (int i = -1; i < 2; i++)
-		{
-			for (int j = -1; j < 2; j++)
-			{
-				if (x + i >= 0 && x + i < fWidth && y + j >= 0 && y + j < fHeight)
-				{
-					if (fField[(y + j)*fWidth + (x + i)] == -1)
-					{
-						surroundingMines++;
-					}
-				}
-			}
-		}
-
-		if (surroundingMines > 0) {
-			button[y * fWidth + x]->SetLabel(std::to_string(surroundingMines));
-		}
-
-	}
 	evt.Skip();
 }
--- Minesweeper/Main.h
+++ Minesweeper/Main.h
@@ -6,10 +6,28 @@ public:
 	Main();
 	~Main();
 
-	int fWidth = 10;
-	int fHeight = 10;
-	wxButton** button;
-	int* fField = nullptr;
+	wxButton* button0 = nullptr;
+	wxButton* button1 = nullptr;
+	wxButton* button2 = nullptr;
+	wxButton* button3 = nullptr;
+	wxButton* button4 = nullptr;
+	wxButton* button5 = nullptr;
+	wxButton* button6 = nullptr;
+	wxButton* button7 = nullptr;
+	wxButton* button8 = nullptr;
+	wxButton* button9 = nullptr;
+	wxButton* buttonDot = nullptr;
+	wxButton* buttonEquals = nullptr;
+	wxButton* buttonClear = nullptr;
+	wxButton* buttonDivide = nullptr;
+	wxButton* buttonSubtract = nullptr;
+	wxButton* buttonAdd = nullptr;
+	wxButton* buttonMulti = nullptr;
+	wxButton* buttonMod = nullptr;
+	wxButton* buttonBinary = nullptr;
+	wxButton* buttonHex = nullptr;
+	wxTextCtrl* text = nullptr;
+	wxStaticText* header = nullptr;
 	bool buttonClick1 = true;
 
 	void OnButtonClick(wxCommandEvent& evt);
